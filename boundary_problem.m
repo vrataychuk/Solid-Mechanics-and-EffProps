abc75d9d9f2e6b97444d2480ce7426578200137a
Maxim Yakovlev <beacon1143@gmail.com>
2020-05-30 01:37:11 +0300

    elastic dynamic 1D

--- boundary_problem.m
+++ boundary_problem.m
@@ -4,6 +4,7 @@ clear, figure(1), clf
 Lx  = 10.0;
 K   = 1.0;
 rho = 1.0;
+G   = 1.5;
 
 % NUMERICS
 Nx  = 100;
@@ -13,18 +14,21 @@ CFL = 1.0;
 % PREPROCESSING
 dX = Lx / (Nx - 1);
 x  = (-Lx / 2) : dX : (Lx / 2);
-dt = CFL * dX / sqrt(K / rho);
+dt = CFL * dX / sqrt( (K + 4*G/3) / rho);
 
 % INITIAL CONDITIONS
-P  = exp(-x .* x);
-P0 = P;
-V  = zeros(1,Nx+1);
+P   = exp(-x .* x);
+P0  = P;
+V   = zeros(1, Nx + 1);
+tau = zeros(1, Nx);
 
 % ACTION LOOP
-for it = 1:Nt
+for it = 1 : Nt
   dPdt       = -diff(V) / dX * K;
   P          = P + dPdt * dt;
-  dVdt       = - diff(P) / dX / rho;
+  dTaudt     = diff(V)/dX * G * 4.0 / 3.0;
+  tau        = tau + dTaudt * dt;
+  dVdt       = diff(-P + tau) / dX / rho;
   V(2:end-1) = V(2:end-1) + dVdt * dt;
 % POSTPROCESSING
   if mod(it, 2) == 0
