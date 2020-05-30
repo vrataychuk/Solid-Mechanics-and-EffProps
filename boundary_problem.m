c509cf1d89c996491a9d58552d6db49e39e0fe60
Maxim Yakovlev <beacon1143@gmail.com>
2020-05-30 03:14:31 +0300

    elastic dynamic 2D

--- boundary_problem.m
+++ boundary_problem.m
@@ -1,36 +1,53 @@
-clear, figure(1), clf
+clear, figure(1), clf, colormap jet
 
 % PHYSICS
 Lx  = 10.0;    % physical length
+Ly  = 10.0;    % physical width
 K   = 1.0;     % bulk modulus
 rho = 1.0;     % density
-G   = 1.5;     % shear modulus
+G   = 0.5;     % shear modulus
 
 % NUMERICS
-Nx  = 100;     % number of space steps
-Nt  = 100;     % number of time steps
-CFL = 1.0;     % Courant–Friedrichs–Lewy
+Nx  = 200;     % number of space steps
+Ny  = 200;
+Nt  = 200;     % number of time steps
+CFL = 0.5;     % Courant–Friedrichs–Lewy
 
 % PREPROCESSING
-dX = Lx / (Nx - 1);                          % space step
-x  = (-Lx / 2) : dX : (Lx / 2);              % space discretization
-dt = CFL * dX / sqrt( (K + 4*G/3) / rho);    % time step
+dX     = Lx / (Nx - 1);                                   % space step
+dY     = Ly/(Ny - 1);
+x      = (-Lx / 2) : dX : (Lx / 2);                       % space discretization
+y      = (-Ly/2) : dY : (Ly/2);
+[x, y] = ndgrid(x, y);                                    % 2D mesh
+dt     = CFL * min(dX, dY) / sqrt( (K + 4*G/3) / rho);    % time step
 
 % INITIAL CONDITIONS
-P   = exp(-x .* x);        % hydrostatic stress (ball part of tensor)
-P0  = P;                   % initial hydrostatic stress
-V   = zeros(1, Nx + 1);    % velocity
-tau = zeros(1, Nx);        % deviatoric stress
+P     = zeros(Nx, Ny);
+P     = exp(-x .* x - y .* y);    % hydrostatic stress (ball part of tensor)
+P0    = P;                        % initial hydrostatic stress
+Vx    = zeros(Nx + 1, Ny);        % velocity
+Vy    = zeros(Nx, Ny + 1);
+tauxx = zeros(Nx, Ny);            % deviatoric stress
+tauyy = zeros(Nx, Ny);
+tauxy = zeros(Nx - 1, Ny - 1);
 
 % ACTION LOOP
 for it = 1 : Nt
-  P          = P + (-diff(V) / dX * K)                  * dt;
-  tau        = tau + (diff(V)/dX * G * 4.0 / 3.0)       * dt;
-  V(2:end-1) = V(2:end-1) + (diff(-P + tau) / dX / rho) * dt;
+  % velocity divergence
+  divV                = diff(Vx,1,1)/dX + diff(Vy,1,2)/dY;
+  P                   = P     + (-divV * K) * dt;
+  tauxx               = tauxx + ((diff(Vx,1,1)/dX - divV/3.0) * G * 2.0) * dt;
+  tauyy               = tauyy + ((diff(Vy,1,2)/dY - divV/3.0) * G * 2.0) * dt;
+  tauxy               = tauxy + ((diff(Vx(2:end-1,:), 1, 2)/dY + diff(Vy(:,2:end-1), 1, 1)/dX) * G) * dt;
+  Vx(2:end-1,2:end-1) = Vx(2:end-1,2:end-1) + (diff(-P(:,2:end-1) + tauxx(:,2:end-1), 1, 1)/dX / rho + diff(tauxy,1,2)/dY)    * dt;
+  Vy(2:end-1,2:end-1) = Vy(2:end-1,2:end-1) + (diff(-P(2:end-1,:) + tauyy(2:end-1,:), 1, 2)/dY / rho + diff(tauxy,1,1)/dX)    * dt;
 % POSTPROCESSING
-  if mod(it, 5) == 0
-    plot(x, P0, 'g', x, P, 'r')
+  if mod(it, 10) == 0
+    pcolor(x, y, P)
     title(it)
+    shading flat
+    colorbar
+    axis image        % square image
     drawnow
   end
 end
